558d802318886face441d27fc918ef352e5734f8
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-04-29 09:02:41 +0100

    Standard error and contrast variance

--- test/fsl/GroundTruth/coordinate_spaces.m
+++ test/fsl/GroundTruth/coordinate_spaces.m
@@ -5,7 +5,8 @@ function coordinate_spaces(featDir)
     coordinate_space_entity(fullfile(featDir, 'mask.nii.gz'), 2, true);
     coordinate_space_entity(fullfile(featDir, 'stats', 'cope1.nii.gz'), 3, true);
     coordinate_space_entity(fullfile(featDir, 'stats', 'varcope1.nii.gz'), 4, true);
-    coordinate_space_entity(fullfile(featDir, 'stats', 'zstat1.nii.gz'), 5, true);
-    coordinate_space_entity(fullfile(featDir, 'stats', 'tstat1.nii.gz'), 6, true);
-    coordinate_space_entity(fullfile(featDir, 'thresh_zstat1.nii.gz'), 7, true);
+    coordinate_space_entity(fullfile(featDir, 'stats', 'sqrt_varcope1.nii.gz'), 5, true);
+    coordinate_space_entity(fullfile(featDir, 'stats', 'zstat1.nii.gz'), 6, true);
+    coordinate_space_entity(fullfile(featDir, 'stats', 'tstat1.nii.gz'), 7, true);
+    coordinate_space_entity(fullfile(featDir, 'thresh_zstat1.nii.gz'), 8, true);
 end
